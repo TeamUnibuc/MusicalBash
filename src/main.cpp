1eaacf9faeb2e6b7e34770230957f8e272825af8
ConstantinDragancea <dragancea.constantin@gmail.com>
2020-05-08 17:39:21 +0300

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "wrapper.hpp"
 #include "console_app.hpp"
 #include "a_logger.hpp"
+#include "sqlite3.h"
 
 using namespace std;
 /// #include "tests/sound_player_t.hpp"
@@ -16,9 +17,19 @@ int main()
     
     // unit_tests::RunAllTests();
 
-    Application musical_bash_app;
+    // Application musical_bash_app;
 
-    musical_bash_app.Run();
+    // musical_bash_app.Run();
+
+    sqlite3* db;
+    int db_res = sqlite3_open("databaseName.db", &db);
+    if (db_res)
+        throw std::runtime_error(sqlite3_errmsg(db));
+    else
+        Logger::Get() << "Opened database successfully" << '\n';
+    
+    sqlite3_close(db);
+    
 
     Logger::Get() << "Closing application..." << '\n';
     return 0;
